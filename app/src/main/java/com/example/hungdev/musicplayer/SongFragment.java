5b6fb5b25edd940cce6f2cbb2040c26080f958a0
havanhung <havanhung0402@gmail.com>
2018-07-24 11:28:03 +0700

    design TabLayou and ViewPager

---
+++ app/src/main/java/com/example/hungdev/musicplayer/SongFragment.java
@@ -0,0 +1,37 @@
+package com.example.hungdev.musicplayer;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SongFragment extends Fragment {
+
+    public SongFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_song, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
---
+++ app/src/main/java/com/example/hungdev/musicplayer/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.hungdev.musicplayer;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hungdev on 24/07/2018.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter{
+    List<Fragment> mFlagmentList = new ArrayList<>();
+    List<String> mFlagmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFlagmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFlagmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        mFlagmentList.add(fragment);
+        mFlagmentTitleList.add(title);
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFlagmentTitleList.get(position);
+    }
+}
